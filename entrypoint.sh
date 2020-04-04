5999c8ad6147b83ba41a92151f99d8a27a0dd6ee
senfung <huisenfung@gmail.com>
2020-04-04 22:21:35 +0800

    test: use default path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,15 @@ set -e
 pwd
 echo $1
 echo $2
-if [[ ! -z "$2" ]]; then
-	cd ..
-	mv workspace $2
-	ls
-	cd $2
-	pwd
-fi
+
+cd ..
+mv workspace $2
+ls
+cd $2
+pwd
 
 sh -c "protoc $1"
-if [[ ! -z "$2" ]]; then
-	cd ..
-	mv $2 workspace
-	cd workspace
-fi
+
+cd ..
+mv $2 workspace
+cd workspace
