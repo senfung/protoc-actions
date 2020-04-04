ccf85898ebfd12d5a12d48a6d92f9095827148a2
senfung <huisenfung@gmail.com>
2020-04-04 23:13:07 +0800

    test: test paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,22 @@ pwd
 echo $1
 echo $2
 ls
-echo "==============="
+echo "=======CREATED DIR========"
 mkdir $2
 ls
 pwd
 find . ! -name . -prune ! -name $2 -exec mv {} $2/ \;
-echo "==============="
-cd github/workspace/$2
+echo "=======MOVIED========"
+ls ./$
+echo "=======CHECK========"
+ls ./$2
+echo "=======GO IN========"
+cd ./$2
 ls
 pwd
 sh -c "protoc $1"
-echo "==============="
+echo "=======EXECUTED========"
+echo "=======BACK OUT+======="
 mv * ../
 cd ..
 pwd
