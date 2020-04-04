2d11ceb8a3e156a8ddc902e26cc9223e104426a2
senfung <huisenfung@gmail.com>
2020-04-04 22:55:30 +0800

    test: see paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,18 @@ set -e
 pwd
 echo $1
 echo $2
-
+ls
+echo "==============="
 mkdir $2
+ls
+pwd
 find . ! -name . -prune ! -name $2 -exec mv {} $2/ \;
+echo "==============="
 cd $DUMDIR
+ls
 pwd
 sh -c "protoc $1"
-
+echo "==============="
 mv * ../
 cd ..
 pwd
