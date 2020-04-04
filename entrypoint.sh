881d44f9009d604881206bf83e890fcdfb950b65
senfung <huisenfung@gmail.com>
2020-04-04 22:17:04 +0800

    test: change execution path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,17 @@ set -e
 pwd
 echo $1
 echo $2
+if [[ ! -z "$2" ]]; then
+	cd ..
+	mv workspace $2
+	ls
+	cd $2
+	pwd
+fi
+
 sh -c "protoc $1"
+if [[ ! -z "$2" ]]; then
+	cd ..
+	mv $2 workspace
+	cd workspace
+fi
